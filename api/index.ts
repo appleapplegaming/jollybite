57807502c52c862fe177ab85f01cd92ad768b8bc
Mustafa <549453@student.fontys.nl>
2025-06-27 20:43:28 +0200

    Add R2 bucket binding

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from "hono";
-const app = new Hono();
+const app = new Hono<{ Bindings: Env }>();
 console.log("init hono");
-app.get("/admin", (c) => c.text("Hono!"));
+app.get("/admin", (c) => {
+  // c.env.jollybite_bucket.get;
+  return c.text("Hono!!");
+});
 
 export default app;
