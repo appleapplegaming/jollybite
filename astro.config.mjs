17d1dee0e51d9327ab266fd255590aec039e086e
Mustafa <549453@student.fontys.nl>
2025-06-27 20:26:31 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   vite: {
     plugins: [tailwindcss(), cloudflare()],
   },
